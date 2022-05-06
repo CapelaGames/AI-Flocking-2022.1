b2b665a48c575ff640b02236cd9816370b4fb979
CapelaGames <andrew@Capelagames.com.au>
2022-05-06 14:27:22 +1000

    set up

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockAgent agentPrefab;
+    public List<FlockAgent> agents;// = new List<FlockAgent>();
+
+    public FlockBehaviour behavior;
+
+    [Range(10, 500)]
+    public int startingCount = 250;
+    public float agentDensity = 0.08f;
+
+    [Range(1f, 100f)]
+    public float driveFactor = 10f;
+    [Range(1f, 100f)]
+    public float maxSpeed = 5f;
+    [Range(1f, 10f)]
+    public float neighborRadius = 1.5f;
+    [Range(0f, 1f)]
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+    float _squareMaxSpeed;
+    float _squareNeighborRadius;
+    float _squareAvoidanceRadius;
+
+
+
+}
---
+++ Assets/Scripts/FlockAgent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class FlockAgent : MonoBehaviour
+{
+    //Flock agentsFlock
+
+    private Collider2D _agentCollider;
+    public Collider2D AgentCollider { get => _agentCollider; }
+
+    void Start() => _agentCollider = GetComponent<Collider2D>();
+
+
+    public void Move(Vector2 velocity)
+    {
+        transform.up = velocity.normalized; //rotate the AI
+        transform.position += (Vector3)velocity * Time.deltaTime; //move the AI
+    }
+}
